a537cd991b6a997f82d31da8de619181496e0953
EC2 Default User <ec2-user@ip-172-31-12-66.us-east-2.compute.internal>
2018-12-15 05:06:03 +0000

    SVGでのりしろ部分を表現しました。

--- js/rect_js.js
+++ js/rect_js.js
@@ -3,9 +3,12 @@ document.getElementById("svg_change").addEventListener("click", function(){
     var y = document.getElementById("svg_width").value * 10;
     console.log(x,y);
 
-    var totebag = document.getElementById("tote_bag1");
-    totebag.setAttribute("fill", "#ff69b4"); // 緑色で塗りつぶす
-    totebag.setAttribute("stroke", "#dc143c");    // 青色の線にする
-    totebag.setAttribute("height", x);
-    totebag.setAttribute("width", y);
+        var totebag1_inside = document.getElementById("totobag1_inside");
+        totebag1_inside.setAttribute("height", x);
+        totebag1_inside.setAttribute("width", y);
+    
+        var totebag1_outside = document.getElementById("totobag1_outside");
+        totebag1_outside.setAttribute("height", x + 20);
+        totebag1_outside.setAttribute("width", y + 20 );    
+
     }, true);
